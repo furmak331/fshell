267d1ec9b6e3b30aabbae13b99ac32d1442977a6
furqan <furmak331@gmail.com>
2025-04-29 07:50:04 +0530

    Add basic shell prompt and input reading

--- main.c
+++ main.c
@@ -1,8 +1,36 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PROMPT "fShell$ "
+#define PROMPT_LENGTH 8
+#define BUFFER_SIZE 1024
 
 int main(int argc, char *argv[])
 {
-        (void)argc, (void)argv;
-        write(STDOUT_FILENO, "fShell$ ", 8);
-        return (0);
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+    
+    (void)argc, (void)argv;
+
+    while (1) {
+
+        write(STDOUT_FILENO, PROMPT, PROMPT_LENGTH);
+
+        bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+
+        if (bytes_read <= 0) {
+            write(STDOUT_FILENO, "\nExiting shell...\n", 18);
+            break;
+        }
+        
+
+        if (bytes_read > 0 && buffer[bytes_read - 1] == '\n') {
+            buffer[bytes_read - 1] = '\0';
+        }
+        
+        printf("You entered: %s\n", buffer);
+    }
+    
+    return (0);
 }
